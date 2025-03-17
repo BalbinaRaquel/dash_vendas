419e9e4275d16792b3fbe9af057444f8816d510b
BalbinaRaquel <balbina.rakell@outlook.com>
2025-03-17 18:20:03 -0300

    Análise vendas por Loja

--- Dashboard_vendas.py
+++ Dashboard_vendas.py
@@ -1,5 +1,5 @@
 
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
 
@@ -12,7 +12,8 @@ df = pd.read_excel('Vendas.xlsx')
 # Criando gráfico de barras
 fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
 
-
+opcoes = list(df['ID Loja'].unique())
+opcoes.append('Todas as Lojas')
 
 app.layout = html.Div(children=[
     html.H1(children='Faturamento das Lojas'),
@@ -21,12 +22,31 @@ app.layout = html.Div(children=[
     html.Div(children='''
         Esse gráfico mostra a quantidade de produtos vendidos, não o faturamento.
     '''),
+    html.Div(id='Texto'),
+
+    dcc.Dropdown(opcoes, value='Todas as Lojas', id='lista_lojas'),
 
     dcc.Graph(
-        id='example-graph',
+        id='Gráfico_Quantidade_Vendas',
         figure=fig
     )
 ])
+
+
+# call back
+@app.callback(
+    Output('Gráfico_Quantidade_Vendas', 'figure'),
+    Input('lista_lojas', 'value')
+)
+def update_output(value):
+    if value == "Todas as Lojas":
+        fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+    else:
+        tabela_filtro = df.loc[df['ID Loja'] == value, :]
+        fig = px.bar(tabela_filtro, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+    return fig
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
